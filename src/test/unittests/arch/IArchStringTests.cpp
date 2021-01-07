f130911f54cead64fd57f71ee82ee70ca4d8b800
Ignacio Rodriguez <ignacionr@gmail.com>
2021-01-07 16:55:56 +0700

    including terminator on translation count

--- src/test/unittests/arch/IArchStringTests.cpp
+++ src/test/unittests/arch/IArchStringTests.cpp
@@ -32,7 +32,7 @@ TEST(IArchStringTests, convStringWCToMB_will_work_do_simple_conversions)
     bool errors;
     auto converted = as.convStringWCToMB(buff, L"Hello", 6, &errors);
     EXPECT_STREQ(buff, "Hello");
-    EXPECT_EQ(converted, 5);
+    EXPECT_EQ(converted, 6);
     EXPECT_EQ(errors, false);
 }
 
@@ -43,6 +43,6 @@ TEST(IArchStringTests, convStringMBToWC_will_work_do_simple_conversions)
     bool errors;
     auto converted = as.convStringMBToWC(buff, "Hello", 6, &errors);
     EXPECT_STREQ(buff, L"Hello");
-    EXPECT_EQ(converted, 5);
+    EXPECT_EQ(converted, 6);
     EXPECT_EQ(errors, false);
 }
